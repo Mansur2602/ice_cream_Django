6185272011262e46f828175fb31f8fe4caf5e7cd
Кали Карим <kalikarim@MacBook-Pro-Kali.local>
2024-11-22 16:59:10 +0500

    Добавил валидацию

--- ice_cream_app/forms.py
+++ ice_cream_app/forms.py
@@ -18,7 +18,7 @@ from captcha.fields import CaptchaField
 class IceCreamForm(forms.Form):
     name = forms.CharField(max_length=100, label="Название")
     flavor = forms.CharField(max_length=100, label="Вкус")
-    price = forms.DecimalField(max_digits=5, decimal_places=2, label="Цена")
+    price = forms.DecimalField( decimal_places=2, label="Цена")
     captcha = CaptchaField()
 
 
@@ -26,4 +26,19 @@ class IceCreamForm(forms.Form):
         price = self.cleaned_data.get('price')
         if price <= 0:
             raise forms.ValidationError("Цена должна быть больше нуля.")
-        return price
+        if price > 1000:
+            raise forms.ValidationError("Цена не может быть больше 1000.")
+        
+    def clean_flavor(self):
+        flavor = self.cleaned_data.get('flavor')
+        name = self.cleaned_data.get('name')
+        if len(flavor) < 3:
+            raise forms.ValidationError("Вкус должен содержать не менее 3 символов.")
+        if flavor.lower() == name.lower():
+            raise forms.ValidationError("Вкус не может быть одинаковым с названием.")
+        return flavor
+    
+
+    
+
+
